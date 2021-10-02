67635f951ed1be09db78c1ea93017564844d8256
Mark <marklim4@gmail.com>
2021-10-02 16:12:49 -0400

    Added css folder, page created navbarDD file for future editing started messing around with user page stuff and bootstrap added some example images installed sass

--- mern/client/src/components/user_page.js
+++ mern/client/src/components/user_page.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import '../CSS/user_page.scss'
+import example from "../imgs/example_1.png"
 // This will require to npm install axios
 import axios from 'axios';
 
@@ -8,18 +9,31 @@ export default class UserPage extends Component {
     render() {
         return (
             <div>
-            <div className = "border border-dark" style={{ margin: 20}}>
+            <div  style={{ margin: 20}}>
                 <button type = "button" class = "btn - btn-outline-primary btn-lg" >Create a New Project</button>
             </div>
-            <div className = "border border-dark" style={{margin:20}}>
-                <div className="container">
 
-                </div>
-                <div className="container">
 
-                </div>
+            <div style={{margin:20}}>
                 <div className="container">
-
+                    <div className="row">
+                        <div className="col">
+                            <div className="container">
+                                <img src={example} className="img-thumbnail"/>
+                                <i className="bi bi-gear"></i>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="container">
+                                <img src={example} className="img-thumbnail"/>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="container">
+                                <img src={example} className="img-thumbnail"/>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             </div>
