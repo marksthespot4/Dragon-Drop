9aba4d420058ad6bb785f94c0b0c800b699a2609
Lucy Lu Cheng <cheng448@purdue.edu>
2021-10-12 12:16:38 -0400

    trying to fix undefined input values

--- mern/client/src/components/home.js
+++ mern/client/src/components/home.js
@@ -11,7 +11,7 @@ import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 import { NavLink } from "react-router-dom";
 import CloseButton from 'react-bootstrap/CloseButton'
-import { getUser } from "./user";
+import { getUser, uploadUser } from "./user";
 
 export default class Home extends Component {
     constructor(props) {
@@ -21,50 +21,48 @@ export default class Home extends Component {
             show: false,
             activeModal: "",
             name: "",
-            modalInputName: "",
             email: "",
             password: "",
+            confirmPassword: ""
         };
+
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        //this.handleConfirmPasswordChange = this.handleConfirmPasswordChange(this);
     }
 
-    //example from https://www.mongodb.com/languages/mern-stack-tutorial
-    /*onSubmit(e) {
-        e.preventDefault();
-        const newEditedperson = {
-          person_name: this.state.person_name,
-          person_position: this.state.person_position,
-          person_level: this.state.person_level,
-        };
-        console.log(newEditedperson);
-     
-        // This will send a post request to update the data in the database.
-        axios
-          .post(
-            "http://localhost:5000/login/" + this.props.match.params.id,
-            newEditedperson
-          )
-          .then((res) => console.log(res.data));
-     
-        this.props.history.push("/");
-    } */
-    onSubmit(e) {
-        console.log("submitted");
-        console.log(this.state.email);
-        this.modalClose(e);
-     
-        getUser(this.props.match.params.id).then(res=> console.log(res));
-     
-        this.props.history.push("/");
-    }
-
-    handleChange(e) {
-        const target = e.target;
-        const name = target.name;
-        const value = target.value;
+    handleEmailChange(e) {
+       /* const target = e.target;
+        console.log(target);
+        alert(target);
+        alert(e.text);
+        const email = target.email;
+        alert("hello");
+        alert(email);
 
         this.setState({
-            [name]: value
-        });
+            email: email,
+        });*/
+        alert(e);
+    }
+
+    handlePasswordChange(e) {
+        const target = e.target;
+        const password = target.password;
+        console.log(password);
+
+        this.setState({
+            password: password,
+        })
+    }
+
+    handleConfirmPasswordChange(e) {
+        const target = e.target;
+        const confirmPassword = target.confirmPassword;
+
+        this.setState({
+            confirmPassword: confirmPassword,
+        })
     }
 
     // handleSubmit(e) {
@@ -78,9 +76,53 @@ export default class Home extends Component {
 
     modalClose() {
         this.setState({
-            modalInputName: "",
+            email: "",
+            password: "",
+            confirmPassword: "",
             show: false,
             activeModal: ""
+        });
+    }
+
+    modalSignup() {
+      /*  if (this.password != this.confirmPassword) { // Passwords don't match
+
+        }
+        else if (this.password.length < 8) { // Password too short
+
+        }
+        else if (!this.password.includes('!') && !this.password.includes('@') && !this.password.includes('#') 
+                && !this.password.includes('$') && !this.password.includes('%') && !this.password.includes('^') 
+                && !this.password.includes('&') && !this.password.includes('*')) { // Password doesn't contain any special characters
+
+        }
+        else if (this.password == this.password.toUpperCase() || this.password == this.password.toLowerCase()) { // Password doesn't have upper and lowercase characters
+
+        }
+        else {
+            uploadUser(this.email, this.password, 0);
+            this.modalClose();
+            this.props.history.push("/user_page");
+        } */
+        //remove this later when the password verification is fixed
+        console.log("before upload");
+        uploadUser(this.email, this.password, 0);
+        this.modalClose();
+        //this.props.history.push("/user_page");
+    }
+
+    modalLogin() {
+        getUser(this.email).then(data =>{
+            if (data == null) { // Account was not found
+
+            }
+            else if (data.password == this.password) { // Account was found, password was correct
+                this.modalClose();
+                this.props.history.push("/user_page");
+            }
+            else { // Account was found, password was incorrect
+
+            }
         });
     }
 
@@ -130,26 +172,24 @@ export default class Home extends Component {
                         <Modal.Title id="contained-modal-title-vcenter">
                             Log In
                         </Modal.Title>
-                        <CloseButton onClick={e => this.onSubmit(e)}></CloseButton>
+                        <CloseButton onClick={e => this.modalClose(e)}></CloseButton>
                     </Modal.Header>
                     <Modal.Body>
                         <h6>Email</h6>
                         <input
                             type="email"
-
-                            // value={this.state.modalInputName}
-                            // name="modalInputName"
-                            // onChange={e => this.handleChange(e)}
+                            value={this.state.email}
+                            name="email"
+                            onChange={e => this.handleEmailChange(e)}
                             // className="form-control"
                         />
 
                         <h6>Password</h6>
                         <input
                             type="password"
-
-                            // value={this.state.modalInputName}
-                            // name="modalInputName"
-                            // onChange={e => this.handleChange(e)}
+                            value={this.state.password}
+                            name="password"
+                            onChange={e => this.handlePasswordChange(e)}
                             // className="form-control"
                         />
                     </Modal.Body>
@@ -158,7 +198,7 @@ export default class Home extends Component {
                             Don't have a Dragon Drop account?
                         </Button>
                         <NavLink className="navbar-brand" to="/user_page" className="nav">
-                            <Button onClick={e => this.modalClose(e)}>
+                            <Button onClick={() => this.modalLogin()}>
                                     Log In
                             </Button>
                         </NavLink>
@@ -182,17 +222,18 @@ export default class Home extends Component {
                         <h6>Email</h6>
                         <input
                             type="email"
-                            // name="modalInputName"
-                            // onChange={e => this.handleChange(e)}
+                            value={this.state.email}
+                            name="email"
+                            onChange={text => this.handleEmailChange(value)}
                             // className="form-control"
                         />
 
                         <h6>Password</h6>
                         <input
                             type="password"
-                            // value={this.state.modalInputName}
-                            // name="modalInputName"
-                            // onChange={e => this.handleChange(e)}
+                            value={this.state.password}
+                            name="password"
+                            onChange={e => this.handlePasswordChange(e)}
                             // className="form-control"
                         />
 
@@ -214,9 +255,9 @@ export default class Home extends Component {
                         <h6>Confirm Password</h6>
                         <input
                             type="password"
-                            // value={this.state.modalInputName}
-                            // name="modalInputName"
-                            // onChange={e => this.handleChange(e)}
+                            value={this.state.confirmPassword}
+                            name="confirmPassword"
+                            onChangeText={e => this.handleConfirmPasswordChange(e)}
                             // className="form-control"
                         />
                     </Modal.Body>
@@ -225,7 +266,7 @@ export default class Home extends Component {
                             Already have a Dragon Drop account?
                         </Button>
                         <NavLink className="navbar-brand" to="/user_page" className="nav">
-                            <Button onClick={e => this.modalClose(e)}>
+                            <Button onClick={() => this.modalSignup()}>
                                 Sign Up
                             </Button>
                         </NavLink>                  
