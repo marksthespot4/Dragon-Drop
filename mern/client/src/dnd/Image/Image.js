b4fe92a3a8ffdf1da68dcad9ce0a876c5f3a8da6
bdtrent <anciltrent@gmail.com>
2021-10-21 19:23:21 -0400

    Testing interactjs in image.js

--- mern/client/src/dnd/Image/Image.js
+++ mern/client/src/dnd/Image/Image.js
@@ -1,23 +1,41 @@
 import DND_logo from "../../images/dragondrop.png"
 import "../styles/image.css"
 import { useState } from "react"
-import {Rnd} from "react-rnd"
+import interact from 'interactjs'
 
 export const Image = ({imageUrl}) => {
     const update = () => (
         console.log(imageUrl)
     )
 
-    return <Rnd 
-    default={{
-        x: 0,
-        y: 0,
-        width: 200,
-        height: 200,
-      }}
-    >
-        <div>
+    const position = {x: 0, y: 0};
+
+    interact('.image')
+    .draggable({
+        inertia: true,
+        modifiers: [
+            interact.modifiers.restrictRect({
+                restriction: 'parent',
+                endOnly: true
+            })
+        ],
+
+        autoScroll: true,
+
+        listeners: {
+            start (event) {
+                console.log(event.type, event.target)
+            },
+            move (event) {
+                position.x += event.dx;
+                position.y += event.dy;
+
+                event.target.style.transform = 'translate(${position.x}px, ${position.y}px)';
+            },
+        }
+    });
+
+    return <div>
             <img src={imageUrl} onClick={update} className="image" alt={"Enter valid url idiot"}/>
         </div>
-        </Rnd>
 }
