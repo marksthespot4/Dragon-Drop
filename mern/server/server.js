6647dc71794531beded05cd1b76b009792945a45
Mark <marklim4@gmail.com>
2021-10-30 21:38:35 -0400

    fixed the CORS issue lol

--- mern/server/server.js
+++ mern/server/server.js
@@ -5,10 +5,10 @@ const passport = require("passport");
 
 const users = require("./routes/users");
 const app = express();
-//const cors = require("cors");
+const cors = require("cors");
 //require("dotenv").config({ path: "./config.env" });
 //const port = process.env.PORT || 5000;
-//app.use(cors());
+app.use(cors());
 //app.use(express.json());
 //app.use(require("./routes/record"));
 // get driver connection
