4e0332031df493e0a6cddfa8ee7707ee7849be30
charliehyun <chyun@purdue.edu>
2021-10-31 22:24:16 -0400

    resolve merge conflicts with main

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -14,6 +14,7 @@ import Save from "./components/save";
 import View from "./components/view"
 import Header from "./components/header";
 import Footer from "./components/footer";
+import MyBuilder from "./dnd/MyBuilder"
 
 const App = () => {
 
