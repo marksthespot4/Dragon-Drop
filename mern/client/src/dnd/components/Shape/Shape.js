bf04b158b25c26aa3e195bcec2a7949b28ffcc33
Mark <marklim4@gmail.com>
2021-11-05 18:18:46 -0400

    readjusted shape to not work with color.

--- mern/client/src/dnd/components/Shape/Shape.js
+++ mern/client/src/dnd/components/Shape/Shape.js
@@ -3,7 +3,7 @@ import {useState} from "react"
 import {Button} from "@material-ui/core"
 import {Rnd} from 'react-rnd';
 import { Redirect } from 'react-router-dom';
-document.addEventListener('contextmenu', function(event){event.preventDefault();})
+window.document.addEventListener('contextmenu', function(event){event.preventDefault();})
 export const Shape = ({
                         shapeType = "Rectangle",
                         shapeText = "http://google.com",
@@ -29,34 +29,35 @@ export const Shape = ({
             alert("Please enter a valid http url.");
         }
     }
-    const returnShape = () => {
-        if (shapeType=="Triangle")
-        {
-            return <Triangle onContextMenu={() => openTab()} width = {widthProp} height={heightProp} fill={{color:color}}>
 
-            </Triangle>
-
-        }
-        else if (shapeType=="Circle")
-        {
-            return <Circle onContextMenu={() => openTab()}  r={radius} fill={{color:color}}>
-
-            </Circle>
-        }
-        else if (shapeType=="Rectangle")
-        {
-            return <Rectangle onContextMenu={() => openTab()}  width={widthProp} height={heightProp} fill={{color:color}}>
-
-            </Rectangle>
-        }
-        else if (shapeType=="Square") {
-            return <img onContextMenu={() => openTab()} height={100} width={100} src={"https://static.vecteezy.com/system/resources/previews/001/209/957/non_2x/square-png.png"} ></img>
-        }
-        else if (shapeType=="Star") {
-            return <img onContextMenu={() => openTab()} height={100} width={100} src={"https://upload.wikimedia.org/wikipedia/commons/b/bf/A_Black_Star.png"}/>
-        }
-    }
+    // const returnShape = () => {
+    //     if (shapeType=="Triangle")
+    //     {
+    //         return <Triangle onContextMenu={() => openTab()} width = {widthProp} height={heightProp} fill={{color:color}}>
+    //
+    //         </Triangle>
+    //
+    //     }
+    //     else if (shapeType=="Circle")
+    //     {
+    //         return <Circle onContextMenu={() => openTab()}  r={radius} fill={{color:color}}>
+    //
+    //         </Circle>
+    //     }
+    //     else if (shapeType=="Rectangle")
+    //     {
+    //         return <Rectangle onContextMenu={() => openTab()}  width={widthProp} height={heightProp} fill={{color:color}}>
+    //
+    //         </Rectangle>
+    //     }
+    //     else if (shapeType=="Square") {
+    //         return <img onContextMenu={() => openTab()} height={100} width={100} src={"https://static.vecteezy.com/system/resources/previews/001/209/957/non_2x/square-png.png"} ></img>
+    //     }
+    //     else if (shapeType=="Star") {
+    //         return <img onContextMenu={() => openTab()} height={100} width={100} src={"https://upload.wikimedia.org/wikipedia/commons/b/bf/A_Black_Star.png"}/>
+    //     }
+    // }
     return <div className="shape">
-        {returnShape()}
+        <img onContextMenu={() => openTab()} height={100} width={100} src={shapeType}/>
     </div>
 }
--- mern/client/src/dnd/components/Shape/ShapePanel.js
+++ mern/client/src/dnd/components/Shape/ShapePanel.js
@@ -40,17 +40,17 @@ const ShapePanel = ({id}) => {
             value = {editor.props.shapeType || ''}
             onChange = {editor.handleUpdate}
         >
-            <option value = 'Rectangle'>
+            <option value = 'https://www.clker.com/cliparts/v/C/Y/S/3/P/black-rectangle-hi.png'>
                 Rectangle
             </option>
-            <option value = 'Triangle'>
+            <option value = 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a7cb840e-b11f-4cd7-aec2-c32840386112/d5dg6kv-d32dc285-3b4e-49b6-b3e0-e88b6c9102e1.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTpmaWxlLmRvd25sb2FkIl0sIm9iaiI6W1t7InBhdGgiOiIvZi9hN2NiODQwZS1iMTFmLTRjZDctYWVjMi1jMzI4NDAzODYxMTIvZDVkZzZrdi1kMzJkYzI4NS0zYjRlLTQ5YjYtYjNlMC1lODhiNmM5MTAyZTEucG5nIn1dXX0.oGbJfiTMj4g2HbQ2YuydLTvQLBoHdFUQlPX87IbUR_8'>
                 Triangle
             </option>
-            <option value = 'Circle'>
+            <option value = 'https://i0.wp.com/www.medassurance.com/wp-content/uploads/2015/06/black-circle.png?ssl=1'>
                 Circle
             </option>
-            <option value = 'Square'> Square </option>
-            <option value = 'Star'> Star </option> 
+            <option value = 'https://static.vecteezy.com/system/resources/previews/001/209/957/non_2x/square-png.png'> Square </option>
+            <option value = 'https://upload.wikimedia.org/wikipedia/commons/b/bf/A_Black_Star.png'> Star </option>
         </Select>
         <div>
             Color
