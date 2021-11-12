beb049c92555b68b6a6e4e02d7f885908a3aa974
Mark <marklim4@gmail.com>
2021-11-12 17:22:56 -0500

    added FAQ page.

---
+++ mern/client/src/components/faq.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import Settings from "./settings";
+
+
+class Faq extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+}
+export default Faq;
