2ca703b97991e957e796037805a5175de8c2f021
charliehyun <chyun@purdue.edu>
2021-11-21 16:14:06 -0500

    add email validation to forgot password and refactor code to class component

--- mern/client/src/components/forgot_password.js
+++ mern/client/src/components/forgot_password.js
@@ -1,21 +1,38 @@
 import Button from 'react-bootstrap/Button';
+import { getUser } from "./user";
+import React, { Component } from 'react';
 
-export default (props) => {
+class ForgotPassword extends Component{
     
-  return (
-      <div>
-          <h1>Reset your password</h1>
-          <p>To reset your password, enter your email below and submit. An email will be sent to you with instructions about how to complete the process.</p>
-          Email
-          <input
-            id="userEmail"
-            type="email"
-            placeholder="Email">
-          </input>
-          <Button>
-              Reset Password
-          </Button>
-          
-      </div>
-  );
-};
+    validateEmail(email){
+        getUser(email).then(data =>{
+            if (data == null) { // Account was not found
+                alert("Account under given email not found");
+            }
+            else {
+                //email is good
+            }
+        });
+    }  
+
+    render() {
+        return (
+            <div>
+                <h1>Reset your password</h1>
+                <p>To reset your password, enter your email below and submit. An email will be sent to you with instructions about how to complete the process.</p>
+                Email
+                <input
+                    id="userEmail"
+                    type="email"
+                    placeholder="Email">
+                </input>
+                <Button onClick={() => this.validateEmail(document.getElementById("userEmail").value)}>
+                    Reset Password
+                </Button>
+            
+            </div>
+        );
+    }
+}
+
+export default ForgotPassword;
